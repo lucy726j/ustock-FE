bd4de966ff1c695e13af1b2dd0b0e6e57b9722d6
yzooop <qkrduswn0404@gmail.com>
2024-08-14 16:38:25 +0900

    Feat : 종목 list

---
+++ src/Pages/Nica.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StockList from "../Component/List/StockList";
+
+const Nica: React.FC = () => {
+    return (
+        <>
+            <StockList />
+        </>
+    );
+}
+
+export default Nica;
---
+++ src/data/data.ts
@@ -0,0 +1,94 @@
+import { StockItemProps } from "../constants/interface"
+import Discord from "../img/logo/Discord.png"
+import GitHub from "../img/logo/GitHub.png"
+import Instagram from "../img/logo/Instagram.png"
+import Line from "../img/logo/Line.png"
+import Spotify from "../img/logo/Spotify.png"
+import Telegram from "../img/logo/Telegram.png"
+import TikTok from "../img/logo/TikTok.png"
+import Twitter from "../img/logo/Twitter.png"
+import Whatsapp from "../img/logo/Whatsapp.png"
+import Youtube from "../img/logo/Youtube.png"
+
+export const data : StockItemProps[] = [ 
+    {
+      "id": 1,
+      "name": "디스코드",
+      "logo": Discord,
+      "code": "111111",
+      "price": 11111,
+      "growth": 11.11
+    },
+    {
+      "id": 2,
+      "name": "깃허브",
+      "logo": GitHub,
+      "code": "222222",
+      "price": 22222,
+      "growth": -22.22
+    },
+    {
+      "id": 3,
+      "name": "인스타그램",
+      "logo": Instagram,
+      "code": "333333",
+      "price": 33333,
+      "growth": 33.33
+    },
+    {
+      "id": 4,
+      "name": "라인",
+      "logo": Line,
+      "code": "444444",
+      "price": 44444,
+      "growth": -44.44
+    },
+    {
+      "id": 5,
+      "name": "스포티파이",
+      "logo": Spotify,
+      "code": "555555",
+      "price": 55555,
+      "growth": 55.55
+    },
+    {
+      "id": 6,
+      "name": "텔레그램",
+      "logo": Telegram,
+      "code": "666666",
+      "price": 66666,
+      "growth": -66.66
+    },
+    {
+      "id": 7,
+      "name": "틱톡",
+      "logo": TikTok,
+      "code": "777777",
+      "price": 77777,
+      "growth": 77.77
+    },
+    {
+      "id": 8,
+      "name": "트위터",
+      "logo": Twitter,
+      "code": "888888",
+      "price": 88888,
+      "growth": -88.88
+    },
+    {
+      "id": 9,
+      "name": "왓츠앱",
+      "logo": Whatsapp,
+      "code": "999999",
+      "price": 99999,
+      "growth": 99.99
+    },
+    {
+      "id": 10,
+      "name": "유튜브",
+      "logo": Youtube,
+      "code": "101010",
+      "price": 101010,
+      "growth": -10.1
+    }
+]
