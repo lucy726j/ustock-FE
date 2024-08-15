d6fc2bec0c1b14a506cc11a987daac1d88bc1846
lucy726j <lucy726jeon@gmail.com>
2024-08-15 16:25:14 +0900

    feat: navBar 페이지 이동 기능 추가

--- src/constants/interface.ts
+++ src/constants/interface.ts
@@ -1,8 +1,14 @@
 export interface NewsProps {
-    title: string;
-    content: string;
-    image: string;
-    writer: string;
-    date: Date;
-    company: string;
+  title: string;
+  content: string;
+  image: string;
+  writer: string;
+  date: Date;
+  company: string;
+}
+
+export interface NavBoxProps {
+  id: string;
+  onClick: (id: string) => void;
+  children: React.ReactNode;
 }
