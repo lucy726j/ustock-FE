b1ce1ca3bb2af994ccc32890086b77be263bf4d1
lucy726j <lucy726jeon@gmail.com>
2024-08-15 17:32:50 +0900

    docs: 검색 기능 로직 끄적이기..

--- src/Component/SearchBar/SearchBar.tsx
+++ src/Component/SearchBar/SearchBar.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import searchImg from "../../img/search.png";
+import { useState } from "react";
 
 const SearchBarStyle = styled.div`
   width: 450px;
@@ -24,11 +25,28 @@ const SearchInput = styled.input`
 `;
 
 const SearchBar = () => {
+  const [list, setList] = useState([]);
+  const [keyword, setKeyword] = useState("");
+
+  const handleSearch = (event: any) => {
+    setKeyword(event.target.value.toLowerCase());
+  };
+
+  // api로 종목 리스트 받아서 list 저장하고
+  // onChange할 때마다 filter한 리스트로 setList해서 목록 보여주기
+  // const searched = list.filter((el) => {
+  //   if (!el.name || !el.code) return false;
+  //   return el.name.toLowerCase().includes(keyword) || el.code.includes(keyword);
+  // });
+
   return (
     <div>
       <SearchBarStyle>
         <SearchImg src={searchImg} alt="검색 돋보기 이미지" />
-        <SearchInput placeholder="검색어를 입력해주세요" />
+        <SearchInput
+          placeholder="검색어를 입력해주세요"
+          onChange={handleSearch}
+        />
       </SearchBarStyle>
     </div>
   );
