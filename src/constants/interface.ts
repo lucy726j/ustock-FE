c078a3ce038856de2a1a27bb1cd19c73ba63f462
lucy726j <lucy726jeon@gmail.com>
2024-08-21 19:28:57 +0900

    Feat : 검색 기능 API 연결

--- src/constants/interface.ts
+++ src/constants/interface.ts
@@ -50,15 +50,6 @@ export interface StockItemProps {
   growth: number;
 }
 
-export interface StockItemProps {
-  id: number;
-  name: string;
-  logo: string;
-  code: string;
-  price: number;
-  growth: number;
-}
-
 export interface NavBoxProps {
   id: string;
   onClick: (id: string) => void;
