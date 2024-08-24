f9f5bfb3a0b6a78f6204647eb964f39b38001c0a
lucy726j <lucy726jeon@gmail.com>
2024-08-24 16:53:33 +0900

    Refactoring : 뉴스 페이지 열 때, noopener, noreferrer, nofollow 설정 추가를 위해 <a> 태그로 변경 및 설정

--- src/Component/News/NewsItem.tsx
+++ src/Component/News/NewsItem.tsx
@@ -1,24 +1,38 @@
 import { NewsProps } from "../../constants/interface";
 
-const NewsItem: React.FC<NewsProps> = ({ id, title, publisher, date, img, url }) => {
-    const handleClick = () => {
-        window.location.href = url;
-    }
-    
-    return(
-        <div className="NewsItem" onClick={handleClick}>
-            <div className="news-section">
-                <div className="news-content">{title}</div>
-                <div className="news-info">
-                    <h3>{publisher}</h3>
-                    <p>{date}</p>
-                </div>
-            </div>
-            <div className="img-section">
-                <img src={img}/>
-            </div>
+const NewsItem: React.FC<NewsProps> = ({
+  id,
+  title,
+  publisher,
+  date,
+  img,
+  url,
+}) => {
+  //   const handleClick = () => {
+  //     window.location.href = url;
+  //   };
+
+  return (
+    // noopener, noreferrer, nofollow 설정을 위해서 <a/>으로 변경
+    <a
+      href={url}
+      className="NewsItem"
+      target="_blank"
+      rel="noopener noreferrer nofollow"
+      // onClick={handleClick}
+    >
+      <div className="news-section">
+        <div className="news-content">{title}</div>
+        <div className="news-info">
+          <h3>{publisher}</h3>
+          <p>{date}</p>
         </div>
-    )
-}
+      </div>
+      <div className="img-section">
+        <img src={img} alt="뉴스 이미지" />
+      </div>
+    </a>
+  );
+};
 
 export default NewsItem;
