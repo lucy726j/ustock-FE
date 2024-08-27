bb7f633906883c68ab1d1be2add2c6b72bb84609
lucy726j <lucy726jeon@gmail.com>
2024-08-27 17:02:21 +0900

    pull

--- src/Pages/PortfolioNo.tsx
+++ src/Pages/PortfolioNo.tsx
@@ -1,11 +1,11 @@
-import PfCreate from "../Component/Portfolio/PfCreate"
+import PfCreate from "../Component/Portfolio/PfCreate";
 
 const PortfolioNo = () => {
-    return (
-        <div className="PortfolioNo">
-            <PfCreate/>
-        </div>
-    )
-}
+  return (
+    <div className="PortfolioNo">
+      <PfCreate />
+    </div>
+  );
+};
 
-export default PortfolioNo
+export default PortfolioNo;
