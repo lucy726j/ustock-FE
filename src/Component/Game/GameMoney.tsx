19d1d798600db929fbd386481761e560e3319554
lucy726j <lucy726jeon@gmail.com>
2024-09-20 16:26:59 +0900

    Docs : 실시간 랭킹 현황 탭 만드는 중

--- src/Component/Game/GameMoney.tsx
+++ src/Component/Game/GameMoney.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { usePortfolioStore } from "../../store/usePortfolioStore";
 import { useNavigate } from "react-router-dom";
+import GameViewTab from "./gameViewTab";
 
 const GameMoney = ({
   setBudget,
@@ -13,8 +14,6 @@ const GameMoney = ({
   setBudget: (budget: number) => void;
   budget: number;
 }) => {
-  const header = ["종목명", "평균단가", "주식수", " 현재금액", "수익률"];
-
   const nav = useNavigate();
 
   const [nickname, setNickname] = useState(""); // 닉네임
@@ -25,6 +24,7 @@ const GameMoney = ({
   const [changeRateFromStart, setChangeRateFromStart] = useState(0); // 전체 대비 변동률
   const [holdingList, setHoldingList] = useState<holding[]>([]); // 20xx년 주식가격
   const [showTable, setShowTable] = useState(false);
+  const [tabView, setTabView] = useState("보유주식");
 
   const check = usePortfolioStore((state) => state.check);
 
@@ -111,81 +111,10 @@ const GameMoney = ({
         </Button>
       </BalanceDetailButton>
 
-      {showTable && (
-        <div>
-          <TableContainer>
-            <table style={{ paddingTop: "20px" }}>
-              <thead>
-                <tr>
-                  {header.map((item, index) => (
-                    <th key={index}>{item}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {holdingList.map((holding, index) => (
-                  <tr
-                    key={index}
-                    style={{
-                      background: index % 2 === 0 ? "#ededed" : "white",
-                    }}
-                  >
-                    <td>{holding.stockName}</td>
-                    <td style={{ textAlign: "right" }}>
-                      {formatPrice(holding.average)}
-                    </td>
-                    <td>{holding.quantity}</td>
-                    <td
-                      style={{
-                        color:
-                          holding.profitRate > 0
-                            ? "red"
-                            : holding.profitRate < 0
-                            ? "blue"
-                            : "black",
-                        textAlign: "right",
-                      }}
-                    >
-                      {formatPrice(holding.price)}
-                    </td>
-                    <td
-                      style={{
-                        color:
-                          holding.profitRate > 0
-                            ? "red"
-                            : holding.profitRate < 0
-                            ? "blue"
-                            : "black",
-                      }}
-                    >
-                      {formatChangeRate(holding.profitRate)}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </TableContainer>
-        </div>
-      )}
+      {showTable && <GameViewTab holdingList={holdingList} />}
     </GameMoneyContainer>
   );
 };
-
-const fadeIn = keyframes`
-    from{
-        opacity:0;
-        transform: translateY(10px)
-    }
-    to{
-        opacity:1;
-        transform: translateY(0)
-    }
-`;
-
-const TableContainer = styled.div`
-  animation: ${fadeIn} 0.5s ease-out forwards;
-  opacity: 0;
-`;
 
 const GameMoneyContainer = styled.div`
   width: 85%;
---
+++ src/Component/Game/gameHoldingsView.tsx
@@ -0,0 +1,81 @@
+import styled, { keyframes } from "styled-components";
+import { formatPrice, formatChangeRate } from "../../util/gameUtil";
+import { holding } from "../../constants/interface";
+
+const fadeIn = keyframes`
+    from{
+        opacity:0;
+        transform: translateY(10px)
+    }
+    to{
+        opacity:1;
+        transform: translateY(0)
+    }
+`;
+
+const TableContainer = styled.div`
+  animation: ${fadeIn} 0.5s ease-out forwards;
+  opacity: 0;
+`;
+
+const GameHoldingsView = ({ holdingList }: any) => {
+  const header = ["종목명", "평균단가", "주식수", " 현재금액", "수익률"];
+  return (
+    <div>
+      <TableContainer>
+        <table style={{ paddingTop: "20px" }}>
+          <thead>
+            <tr>
+              {header.map((item, index) => (
+                <th key={index}>{item}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {holdingList.map((holding: any, index: any) => (
+              <tr
+                key={index}
+                style={{
+                  background: index % 2 === 0 ? "#ededed" : "white",
+                }}
+              >
+                <td>{holding.stockName}</td>
+                <td style={{ textAlign: "right" }}>
+                  {formatPrice(holding.average)}
+                </td>
+                <td>{holding.quantity}</td>
+                <td
+                  style={{
+                    color:
+                      holding.profitRate > 0
+                        ? "red"
+                        : holding.profitRate < 0
+                        ? "blue"
+                        : "black",
+                    textAlign: "right",
+                  }}
+                >
+                  {formatPrice(holding.price)}
+                </td>
+                <td
+                  style={{
+                    color:
+                      holding.profitRate > 0
+                        ? "red"
+                        : holding.profitRate < 0
+                        ? "blue"
+                        : "black",
+                  }}
+                >
+                  {formatChangeRate(holding.profitRate)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default GameHoldingsView;
