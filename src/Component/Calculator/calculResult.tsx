35f6506b5692ce4a3ac20c441b7550d5ddb03ea1
yzooop <qkrduswn0404@gmail.com>
2024-09-23 20:50:28 +0900

    Feat : 거래 swipe 수정

--- src/Component/Calculator/calculResult.tsx
+++ src/Component/Calculator/calculResult.tsx
@@ -5,172 +5,196 @@ import Chicken from "../../img/chicken.png";
 import Iphone from "../../img/iphone.png";
 import styled from "styled-components";
 import Skrrr from "../../img/SkerrImg.png";
-import { CalculResultProps, UserProps } from "../../constants/interface";
+import { CalculResultProps } from "../../constants/interface";
 import { ValueProps } from "../../constants/interface";
 import { formatPrice } from "../../util/util";
 
 const Container = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  padding: 50px;
-  border-top: 2px solid rgb(0, 0, 0, 0.2);
-  margin-bottom: 1rem;
-  /* background-color: yellow; */
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    padding: 50px;
+    border-top: 2px solid rgb(0, 0, 0, 0.2);
+    margin-bottom: 1rem;
+    /* background-color: yellow; */
 `;
 
 const ResultContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  margin-top: 20px;
-  position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin-top: 20px;
+    position: relative;
 `;
 
 const DivContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 15px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 15px;
 `;
 
 const ImgStyle = styled.img`
-  width: 25px;
-  height: 25px;
+    width: 25px;
+    height: 25px;
 `;
 
 const SpanContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 3px;
-  font-size: 20px;
-  white-space: nowrap;
+    display: flex;
+    flex-direction: row;
+    gap: 3px;
+    font-size: 20px;
+    white-space: nowrap;
 `;
 const SpanStyle = styled.div<ValueProps>`
-  font-weight: bold;
-  color: ${(props) => (props.$isNegative ? "#615EFC" : "#FF5759")};
+    font-weight: bold;
+    color: ${(props) => (props.$isNegative ? "#615EFC" : "#FF5759")};
 `;
 
 const SkrrrBird = styled.img`
-  position: absolute;
-  top: 45px;
-  left: 115px;
-  width: 260px;
-  opacity: 0.5;
-  z-index: -1;
+    position: absolute;
+    top: 45px;
+    left: 115px;
+    width: 260px;
+    opacity: 0.5;
+    z-index: -1;
 `;
 
 const NeverBuySkrrrBird = styled.img`
-  margin-right: 5rem;
-  width: 460px;
-  opacity: 0.5;
+    margin-right: 5rem;
+    width: 460px;
+    opacity: 0.5;
 `;
 
 const SkrrrText = styled.div`
-  position: absolute;
-  top: 70px;
-  left: 270px;
-  z-index: 3;
-  color: #ff5759;
-  font-size: 25px;
-  white-space: nowrap;
+    position: absolute;
+    top: 70px;
+    left: 270px;
+    z-index: 3;
+    color: #ff5759;
+    font-size: 25px;
+    white-space: nowrap;
 `;
 
 const NeverBuyText = styled.div`
-  /* position: absolute; */
-  top: 670px;
-  left: 470px;
-  z-index: 3;
-  color: #ff5759;
-  font-size: 25px;
-  white-space: nowrap;
+    /* position: absolute; */
+    top: 670px;
+    left: 470px;
+    z-index: 3;
+    color: #ff5759;
+    font-size: 25px;
+    white-space: nowrap;
 `;
 
 const BirdContainer = styled.div`
-  display: flex;
-  height: 400px;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  flex-direction: column;
+    display: flex;
+    height: 400px;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    flex-direction: column;
 `;
 
 const CalculResult: React.FC<CalculResultProps> = ({
-  price,
-  slave,
-  candy,
-  soul,
-  chicken,
-  iphone,
+    price,
+    slave,
+    candy,
+    soul,
+    chicken,
+    iphone,
 }) => {
-  let userName: string = "사용자";
+    let userName: string = "사용자";
 
-  const userData = localStorage.getItem("user");
-  if (userData) {
-    userName = JSON.parse(userData).name;
-  }
+    const userData = localStorage.getItem("user");
+    if (userData) {
+        userName = JSON.parse(userData).name;
+    }
 
-  return (
-    <>
-      {price !== 0 ? (
-        <Container>
-          <SpanContainer>
-            <SpanStyle $isNegative={true}>{userName}</SpanStyle>
-            님은
-            <SpanStyle $isNegative={price < 0}>
-              {formatPrice(Math.abs(price))}
-            </SpanStyle>
-            원을 <span>{price >= 0 ? "벌었습니다." : "잃었습니다.."}</span>
-          </SpanContainer>
-          <ResultContainer>
-            <DivContainer>
-              <ImgStyle src={Salary} alt="" />
-              <SpanContainer>
-                2024년 최저시급 기준{" "}
-                <SpanStyle $isNegative={price < 0}>{slave}</SpanStyle>시간
-              </SpanContainer>
-            </DivContainer>
-            <DivContainer>
-              <ImgStyle src={Candy} alt="" />
-              <SpanContainer>
-                새콤달콤 <SpanStyle $isNegative={price < 0}>{candy}</SpanStyle>
-                개
-              </SpanContainer>
-            </DivContainer>
-            <DivContainer>
-              <ImgStyle src={Soul} alt="" />
-              <SpanContainer>
-                국밥<SpanStyle $isNegative={price < 0}>{soul}</SpanStyle>그릇
-              </SpanContainer>
-            </DivContainer>
-            <DivContainer>
-              <ImgStyle src={Chicken} alt="" />
-              <SpanContainer>
-                치킨 <SpanStyle $isNegative={price < 0}>{chicken}</SpanStyle>
-                마리
-              </SpanContainer>
-            </DivContainer>
-            <DivContainer>
-              <ImgStyle src={Iphone} alt="" />
-              <SpanContainer>
-                아이폰 <SpanStyle $isNegative={price < 0}>{iphone}</SpanStyle>대
-              </SpanContainer>
-            </DivContainer>
-            <SkrrrBird src={Skrrr} alt="" />
-            <SkrrrText>{price >= 0 ? "살껄!@" : "팔껄!@"}</SkrrrText>
-          </ResultContainer>
-        </Container>
-      ) : (
-        <BirdContainer>
-          <NeverBuyText>그 돈으론 1주도 사지 못했~스껄~,,,@</NeverBuyText>
-          <NeverBuySkrrrBird src={Skrrr} alt="" />
-        </BirdContainer>
-      )}
-    </>
-  );
+    return (
+        <>
+            {price !== 0 ? (
+                <Container>
+                    <SpanContainer>
+                        <SpanStyle $isNegative={true}>{userName}</SpanStyle>
+                        님은
+                        <SpanStyle $isNegative={price < 0}>
+                            {formatPrice(Math.abs(price))}
+                        </SpanStyle>
+                        원을{" "}
+                        <span>
+                            {price >= 0 ? "벌었습니다." : "잃었습니다.."}
+                        </span>
+                    </SpanContainer>
+                    <ResultContainer>
+                        <DivContainer>
+                            <ImgStyle src={Salary} alt="" />
+                            <SpanContainer>
+                                2024년 최저시급 기준{" "}
+                                <SpanStyle $isNegative={price < 0}>
+                                    {slave}
+                                </SpanStyle>
+                                시간
+                            </SpanContainer>
+                        </DivContainer>
+                        <DivContainer>
+                            <ImgStyle src={Candy} alt="" />
+                            <SpanContainer>
+                                새콤달콤{" "}
+                                <SpanStyle $isNegative={price < 0}>
+                                    {candy}
+                                </SpanStyle>
+                                개
+                            </SpanContainer>
+                        </DivContainer>
+                        <DivContainer>
+                            <ImgStyle src={Soul} alt="" />
+                            <SpanContainer>
+                                국밥
+                                <SpanStyle $isNegative={price < 0}>
+                                    {soul}
+                                </SpanStyle>
+                                그릇
+                            </SpanContainer>
+                        </DivContainer>
+                        <DivContainer>
+                            <ImgStyle src={Chicken} alt="" />
+                            <SpanContainer>
+                                치킨{" "}
+                                <SpanStyle $isNegative={price < 0}>
+                                    {chicken}
+                                </SpanStyle>
+                                마리
+                            </SpanContainer>
+                        </DivContainer>
+                        <DivContainer>
+                            <ImgStyle src={Iphone} alt="" />
+                            <SpanContainer>
+                                아이폰{" "}
+                                <SpanStyle $isNegative={price < 0}>
+                                    {iphone}
+                                </SpanStyle>
+                                대
+                            </SpanContainer>
+                        </DivContainer>
+                        <SkrrrBird src={Skrrr} alt="" />
+                        <SkrrrText>
+                            {price >= 0 ? "살껄!@" : "팔껄!@"}
+                        </SkrrrText>
+                    </ResultContainer>
+                </Container>
+            ) : (
+                <BirdContainer>
+                    <NeverBuyText>
+                        그 돈으론 1주도 사지 못했~스껄~,,,@
+                    </NeverBuyText>
+                    <NeverBuySkrrrBird src={Skrrr} alt="" />
+                </BirdContainer>
+            )}
+        </>
+    );
 };
 
 export default CalculResult;
--- src/Component/Game/GameButtons.tsx
+++ src/Component/Game/GameButtons.tsx
@@ -5,45 +5,44 @@ import "./GameButtonsStyle.css";
 import { useNavigate, useParams } from "react-router-dom";
 
 interface GameButtonsProps {
-
-  openTradeModal: () => void;
-  openPassModal: () => void;
-  budget?: number;
-  setBudget?: (budget: number) => void;
+    openTradeModal: () => void;
+    openPassModal: () => void;
+    budget?: number;
+    setBudget?: (budget: number) => void;
 }
 
 const GameButtons = ({
-  openTradeModal,
-  openPassModal,
-  budget,
-  setBudget,
+    openTradeModal,
+    openPassModal,
+    budget,
+    setBudget,
 }: GameButtonsProps) => {
-  const nav = useNavigate();
-  const { year } = useParams<{ year: string }>();
+    const nav = useNavigate();
+    const { year } = useParams<{ year: string }>();
 
-  // 정보 거래소로 이동
-  const goToInfoPage = () => {
-    if (year) {
-      nav(`/game/info/${year}`, { state: { budget } });
-    }
-  };
+    // 정보 거래소로 이동
+    const goToInfoPage = () => {
+        if (year) {
+            nav(`/game/info/${year}`, { state: { budget } });
+        }
+    };
 
-  return (
-    <div className="GameButtonsWrapper">
-      <div className="GameButton" onClick={goToInfoPage}>
-        <img src={info}></img>
-        <span>정보거래소</span>
-      </div>
-      <div className="GameButton" onClick={openTradeModal}>
-        <img src={trade}></img>
-        <span>거래하기</span>
-      </div>
-      <div className="GameButton" onClick={openPassModal}>
-        <img src={pass}></img>
-        <span>넘어가기</span>
-      </div>
-    </div>
-  );
+    return (
+        <div className="GameButtonsWrapper">
+            <div className="GameButton" onClick={goToInfoPage}>
+                <img src={info} alt="정보거래소"></img>
+                <span>정보거래소</span>
+            </div>
+            <div className="GameButton" onClick={openTradeModal}>
+                <img src={trade} alt="거래하기"></img>
+                <span>거래하기</span>
+            </div>
+            <div className="GameButton" onClick={openPassModal}>
+                <img src={pass} alt="넘어가기"></img>
+                <span>넘어가기</span>
+            </div>
+        </div>
+    );
 };
 
 export default GameButtons;
