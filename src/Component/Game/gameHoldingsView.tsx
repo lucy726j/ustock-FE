333d9cb24d8e9678ba227ed60c0932990f495040
lucy726j <lucy726jeon@gmail.com>
2024-09-23 17:48:56 +0900

    Docs : 보유한 주식이 없습니다 문구 추가

--- src/Component/Game/gameHoldingsView.tsx
+++ src/Component/Game/gameHoldingsView.tsx
@@ -24,54 +24,60 @@ const GameHoldingsView = ({ holdingList }: any) => {
     <div>
       <TableContainer>
         <table style={{ paddingTop: "20px" }}>
-          <thead>
-            <tr>
-              {header.map((item, index) => (
-                <th key={index}>{item}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {holdingList.map((holding: any, index: any) => (
-              <tr
-                key={index}
-                style={{
-                  background: index % 2 === 0 ? "#ededed" : "white",
-                }}
-              >
-                <td>{holding.stockName}</td>
-                <td style={{ textAlign: "right" }}>
-                  {formatPrice(holding.average)}
-                </td>
-                <td>{holding.quantity}</td>
-                <td
-                  style={{
-                    color:
-                      holding.profitRate > 0
-                        ? "red"
-                        : holding.profitRate < 0
-                        ? "blue"
-                        : "black",
-                    textAlign: "right",
-                  }}
-                >
-                  {formatPrice(holding.price)}
-                </td>
-                <td
-                  style={{
-                    color:
-                      holding.profitRate > 0
-                        ? "red"
-                        : holding.profitRate < 0
-                        ? "blue"
-                        : "black",
-                  }}
-                >
-                  {formatChangeRate(holding.profitRate)}
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          {holdingList.length < 1 ? (
+            <p>아직 보유한 주식이 없습니다!</p>
+          ) : (
+            <>
+              <thead>
+                <tr>
+                  {header.map((item, index) => (
+                    <th key={index}>{item}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {holdingList.map((holding: any, index: any) => (
+                  <tr
+                    key={index}
+                    style={{
+                      background: index % 2 === 0 ? "#ededed" : "white",
+                    }}
+                  >
+                    <td>{holding.stockName}</td>
+                    <td style={{ textAlign: "right" }}>
+                      {formatPrice(holding.average)}
+                    </td>
+                    <td>{holding.quantity}</td>
+                    <td
+                      style={{
+                        color:
+                          holding.profitRate > 0
+                            ? "red"
+                            : holding.profitRate < 0
+                            ? "blue"
+                            : "black",
+                        textAlign: "right",
+                      }}
+                    >
+                      {formatPrice(holding.price)}
+                    </td>
+                    <td
+                      style={{
+                        color:
+                          holding.profitRate > 0
+                            ? "red"
+                            : holding.profitRate < 0
+                            ? "blue"
+                            : "black",
+                      }}
+                    >
+                      {formatChangeRate(holding.profitRate)}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </>
+          )}
         </table>
       </TableContainer>
     </div>
