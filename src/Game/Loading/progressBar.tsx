53e9192ffbc23f4be68b6bf4e274788bd4037748
yeondu._ <dustn2366@naver.com>
2024-09-25 19:50:28 +0900

    Fix :프로그래스바 수정

--- src/Game/Loading/progressBar.tsx
+++ src/Game/Loading/progressBar.tsx
@@ -11,12 +11,14 @@ const Container = styled.div`
   border-radius: 50px;
   padding: 5px;
   display: flex;
+  align-items: center;
   border: 2px solid #ff79c6;
+  height: 30px;
 `;
 
 const Bar = styled.div`
   background: linear-gradient(90deg, #ff79c6, #8be9fd);
-  height: 30px;
+  height: 20px;
   border-radius: 50px;
   transition: width 0.4s ease;
   display: flex;
@@ -31,64 +33,21 @@ const Text = styled.span`
   font-size: 12px;
 `;
 
-const Wrapper = styled.div`
-  width: 100%;
-  display: flex;
-  max-width: 500px;
-  align-items: center;
-  position: relative;
-  transition: left 0.4s ease; /* 부드럽게 이동 */
-  height: 0;
-`;
-
-const Label = styled.p`
-  font-size: 12px;
-  font-weight: bold;
-  padding: 0;
-`;
-
 interface ProgressBarProps {
   progress: number;
 }
 
 const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
-  const playerRef = useRef<Player | null>(null);
-
-  useEffect(() => {
-    if (progress === 0 && playerRef.current) {
-      playerRef.current.stop();
-    }
-  }, [progress]);
-
   return (
-    <>
-      <Wrapper>
-        {/* <Label style={{ marginLeft: `${progress}%` }}>게임 진행도...</Label> */}
-        <Player
-          ref={playerRef}
-          src={Mario}
-          style={{
-            height: "80px",
-            width: "80px",
-            top: "-45px",
-            left: `${progress}%`,
-            position: "absolute",
-            transform: "translateX(-50%)",
-          }}
-          loop
-          autoplay={true}
-        />
-      </Wrapper>
-      <Container>
-        <Bar
-          style={{
-            width: `${progress}%`,
-          }}
-        >
-          <Text>{progress.toFixed(0)}%</Text>
-        </Bar>
-      </Container>
-    </>
+    <Container>
+      <Bar
+        style={{
+          width: `${progress}%`,
+        }}
+      >
+        <Text>{progress.toFixed(0)}%</Text>
+      </Bar>
+    </Container>
   );
 };
 
