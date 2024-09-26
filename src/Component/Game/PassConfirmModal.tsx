4b3f5ac466b33784490402565d8a9be5da1f9ec2
yzooop <qkrduswn0404@gmail.com>
2024-09-26 15:50:34 +0900

    Feat : 마지막 턴에 거래 안되게 하기

--- src/Component/Game/PassConfirmModal.tsx
+++ src/Component/Game/PassConfirmModal.tsx
@@ -5,12 +5,14 @@ interface TradeConfirmModalProps {
     isOpen: boolean;
     onConfirm: () => void;
     onRequestClose: () => void;
+    year: string;
 }
 
 const PassConfirmModal: React.FC<TradeConfirmModalProps> = ({
     isOpen,
     onConfirm,
     onRequestClose,
+    year,
 }) => {
     return (
         <div
@@ -39,9 +41,15 @@ const PassConfirmModal: React.FC<TradeConfirmModalProps> = ({
                         marginTop: "50px",
                     }}
                 >
-                    <p>
-                        거래를 종료하고 <br /> 다음 년도로 넘어가시겠습니까?
-                    </p>
+                    {year === "2023" ? (
+                        <p>
+                            게임이 완료되었습니다. <br /> 최종결과로 넘어갑니다.
+                        </p>
+                    ) : (
+                        <p>
+                            거래를 종료하고 <br /> 다음 년도로 넘어가시겠습니까?
+                        </p>
+                    )}
                 </div>
             </ModalOpen>
         </div>
