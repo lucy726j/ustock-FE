de8bacd1c10efd671bd951db4f1cf3a392569c42
yeondu._ <dustn2366@naver.com>
2024-09-30 20:48:41 +0900

    Feat: Button 컴포넌트 disabled 속성 추가/ 정보거래소 종목 선택 안했을 시 버튼 활성화 x

--- src/constants/interface.ts
+++ src/constants/interface.ts
@@ -1,272 +1,273 @@
 export type ButtonStyleProps = {
-    $state: "normal";
-    $size: "small" | "medium" | "large" | "plusBtn" | "gradientBtn";
-    $colorType: "main" | "gradient" | "cancel" | "stroke";
+  $state: "normal";
+  $size: "small" | "medium" | "large" | "plusBtn" | "gradientBtn";
+  $colorType: "main" | "gradient" | "cancel" | "stroke";
+  disabled?: boolean;
 };
 
 export interface ButtonProps extends ButtonStyleProps {
-    onClick?: () => void;
-    children: React.ReactNode;
+  onClick?: () => void;
+  children: React.ReactNode;
 }
 
 export type InputStyleProps = {
-    size: "small" | "medium" | "large" | "nickname";
-    colorType: "fillType" | "strokeType";
-    disabled?: boolean;
+  size: "small" | "medium" | "large" | "nickname";
+  colorType: "fillType" | "strokeType";
+  disabled?: boolean;
 };
 
 export interface inputProps extends InputStyleProps {
-    placeholder: string;
-    value?: string;
-    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
-    name?: string;
-    icon?: React.ReactNode;
-    isValid: boolean;
-    errorMessage: string;
-    maxLength?: number;
+  placeholder: string;
+  value?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
+  name?: string;
+  icon?: React.ReactNode;
+  isValid: boolean;
+  errorMessage: string;
+  maxLength?: number;
 }
 
 export interface ModalProps {
-    title?: string;
-    isOpen: boolean;
-    onRequestClose?: () => void;
-    onConfirm?: () => void;
-    confirmLabel?: string;
-    cancelLabel?: string;
-    showConfirmButton?: string;
-    showCancelButton?: boolean;
-    showOneConfirmBtn?: boolean;
-    children?: React.ReactNode;
-    text?: string;
-    icon?: string;
+  title?: string;
+  isOpen: boolean;
+  onRequestClose?: () => void;
+  onConfirm?: () => void;
+  confirmLabel?: string;
+  cancelLabel?: string;
+  showConfirmButton?: string;
+  showCancelButton?: boolean;
+  showOneConfirmBtn?: boolean;
+  children?: React.ReactNode;
+  text?: string;
+  icon?: string;
 }
 
 export interface ListProps {
-    portfolioId: number;
-    logo: string;
-    name: string;
-    code: string;
-    price: number;
-    changeRate: number;
+  portfolioId: number;
+  logo: string;
+  name: string;
+  code: string;
+  price: number;
+  changeRate: number;
 }
 
 export interface PlusProps {
-    portfolioId: number;
-    name: string;
-    logo: string;
-    code: string;
-    quantity: number;
-    average: number;
+  portfolioId: number;
+  name: string;
+  logo: string;
+  code: string;
+  quantity: number;
+  average: number;
 }
 
 export interface NavBoxProps {
-    id: string;
-    onClick: (id: string) => void;
-    children: React.ReactNode;
-    $isActive: boolean;
+  id: string;
+  onClick: (id: string) => void;
+  children: React.ReactNode;
+  $isActive: boolean;
 }
 
 export interface SearchBarProps {
-    onSelect: (selected: number) => void;
+  onSelect: (selected: number) => void;
 }
 
 export interface NewsProps {
-    code: string;
-    title: string;
-    publisher: string;
-    date: string;
-    name: string;
-    url: string;
+  code: string;
+  title: string;
+  publisher: string;
+  date: string;
+  name: string;
+  url: string;
 }
 
 export interface ValueProps {
-    $isNegative: boolean;
+  $isNegative: boolean;
 }
 
 export interface ViewSelectProps {
-    isSelected: boolean;
+  isSelected: boolean;
 }
 
 export interface StockDataProps {
-    code: string;
-    name: string;
-    logo: string;
-    price: number;
-    change: number;
-    changeRate: number;
+  code: string;
+  name: string;
+  logo: string;
+  price: number;
+  change: number;
+  changeRate: number;
 }
 
 export interface StockDataPropList {
-    data: Array<StockDataProps>;
+  data: Array<StockDataProps>;
 }
 
 export interface MarketDataProps {
-    kospi: {
-        price: number;
-        change: number;
-        changeRate: number;
-    };
-    kosdaq: {
-        price: number;
-        change: number;
-        changeRate: number;
-    };
+  kospi: {
+    price: number;
+    change: number;
+    changeRate: number;
+  };
+  kosdaq: {
+    price: number;
+    change: number;
+    changeRate: number;
+  };
 }
 
 export interface DropdownProps {
-    dropList: any[];
-    onSelect: (category: string | number) => void;
+  dropList: any[];
+  onSelect: (category: string | number) => void;
 }
 
 export interface StockProps {
-    portfolioId: number;
-    code: string;
-    name: string;
-    quantity: number;
-    average: number;
-    profitRate: number;
-    logo?: string;
+  portfolioId: number;
+  code: string;
+  name: string;
+  quantity: number;
+  average: number;
+  profitRate: number;
+  logo?: string;
 }
 
 export interface PortfolioProps {
-    name: string;
-    budget: number;
-    principal: number;
-    profit: number;
-    profitRate: number;
-    stocks: StockProps[];
+  name: string;
+  budget: number;
+  principal: number;
+  profit: number;
+  profitRate: number;
+  stocks: StockProps[];
 }
 
 export interface StockItemProps {
-    id: number;
-    name: string;
-    code: string;
-    price: number;
-    growth: number;
-    logo: string;
+  id: number;
+  name: string;
+  code: string;
+  price: number;
+  growth: number;
+  logo: string;
 }
 
 export interface CalculResultProps {
-    price: number;
-    slave: string;
-    candy: string;
-    soul: string;
-    iphone: string;
-    chicken: string;
+  price: number;
+  slave: string;
+  candy: string;
+  soul: string;
+  iphone: string;
+  chicken: string;
 }
 
 export interface ChartProps {
-    date: string;
-    candle: {
-        open: number;
-        high: number;
-        low: number;
-        close: number;
-    };
-    news: {
-        title: string;
-        url: string;
-    }[];
+  date: string;
+  candle: {
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+  };
+  news: {
+    title: string;
+    url: string;
+  }[];
 }
 
 export interface CandleData {
-    data: { x: string; y: [number, number, number, number] }[];
+  data: { x: string; y: [number, number, number, number] }[];
 }
 
 export interface UserProps {
-    name: string;
+  name: string;
 }
 
 export interface IconWrapperProps {
-    IconComponent: any;
-    $isActive: boolean;
-    // Type for react-icons components
+  IconComponent: any;
+  $isActive: boolean;
+  // Type for react-icons components
 }
 
 export interface GameHeaderProp {
-    text: String;
+  text: String;
 }
 
 export interface SaySkrrProps {
-    rank: number;
-    money: number;
+  rank: number;
+  money: number;
 }
 
 export interface RankDataProps {
-    nickname: String;
-    total: number;
-    profitRate: number;
-    playerType?: String;
-    index?: number;
+  nickname: String;
+  total: number;
+  profitRate: number;
+  playerType?: String;
+  index?: number;
 }
 
 export interface RankListProps {
-    data: Array<RankDataProps>;
+  data: Array<RankDataProps>;
 }
 
 export interface GameNewsProps {
-    title: string;
-    url: string;
-    publisher: string;
-    date: string;
+  title: string;
+  url: string;
+  publisher: string;
+  date: string;
 }
 
 export interface GameNewsListProps {
-    data: Array<GameNewsProps>;
+  data: Array<GameNewsProps>;
 }
 
 export interface Stock {
-    stockId: number;
-    name: string;
-    prev: number;
-    current: number;
-    change: number;
-    changeRate: number;
+  stockId: number;
+  name: string;
+  prev: number;
+  current: number;
+  change: number;
+  changeRate: number;
 }
 
 export interface StockYearProps {
-    stockId: number;
-    name: string;
-    prev: number;
-    current: number;
-    change: number;
-    changeRate: number;
+  stockId: number;
+  name: string;
+  prev: number;
+  current: number;
+  change: number;
+  changeRate: number;
 }
 
 export interface StocksData {
-    header: string[];
-    data: StockYearProps[];
+  header: string[];
+  data: StockYearProps[];
 }
 
 export interface GameMoneyProps {
-    budget: number;
-    nickname: string;
-    total: number;
-    changeFromLast: number;
-    changeFromStart: number;
-    changeRateFromLast: number;
-    changeRateFromStart: number;
+  budget: number;
+  nickname: string;
+  total: number;
+  changeFromLast: number;
+  changeFromStart: number;
+  changeRateFromLast: number;
+  changeRateFromStart: number;
 }
 
 export interface StocksTableProps {
-    stocks: StockYearProps[];
-    year: string;
+  stocks: StockYearProps[];
+  year: string;
 }
 
 export interface holding {
-    stockId: number;
-    stockName: string;
-    average: number;
-    price: number;
-    quantity: number;
-    profitRate: number;
+  stockId: number;
+  stockName: string;
+  average: number;
+  price: number;
+  quantity: number;
+  profitRate: number;
 }
 
 export interface GameStockProps {
-    stockId: number;
-    fakeName: string;
-    realName: string;
-    chart: { x: string; y: number }[];
-    news: { title: string; url: string; publisher: string; date: string }[];
+  stockId: number;
+  fakeName: string;
+  realName: string;
+  chart: { x: string; y: number }[];
+  news: { title: string; url: string; publisher: string; date: string }[];
 }
