5905ab34f0a3090d868278ec050ce735bc72cc0c
lucy726j <lucy726jeon@gmail.com>
2024-09-30 09:28:43 +0900

    Fix : 캐로셀 버튼 위치 수정

--- src/Component/GameCarousel/gameCarousel.tsx
+++ src/Component/GameCarousel/gameCarousel.tsx
@@ -2,21 +2,22 @@ import { useSpringCarousel } from "react-spring-carousel";
 import CarouselItem from "./carouselItem";
 import styled from "styled-components";
 import { Colors } from "../../Styles/Colors";
-import { GameStockProps } from "../../constants/interface";
+import Button from "../Button/button";
 
 const BtnContainer = styled.div`
   display: flex;
-  gap: 20px;
-  justify-content: center;
+  justify-content: space-between;
+  margin-top: 1rem;
 `;
 const BtnStyle = styled.button`
-  padding: 10px 20px;
+  padding: 7px 15px;
   background-color: ${Colors.main};
   border: 1px solid ${Colors.main};
   border-radius: 5px;
   color: white;
   font-size: 17px;
   font-family: "SCDream6";
+  white-space: nowrap;
 `;
 const ChartStyle = styled.div`
   overflow: hidden;
@@ -33,11 +34,12 @@ const GameCarousel = ({ stocks }: any) => {
 
   return (
     <>
-      <ChartStyle>{carouselFragment}</ChartStyle>
       <BtnContainer>
         <BtnStyle onClick={slideToPrevItem}>이전</BtnStyle>
         <BtnStyle onClick={slideToNextItem}>다음</BtnStyle>
       </BtnContainer>
+      <ChartStyle>{carouselFragment}</ChartStyle>
+    {/* <Button /> */}
     </>
   );
 };
